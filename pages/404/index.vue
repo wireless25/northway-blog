59d4fd262a5ab5a2a40420086e04f322a144d30f
Stephan <stephan.simonett@gmail.com>
2019-05-21 22:49:52 +0200

    404

---
+++ pages/404/index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <section class="container">
+    <h1>404</h1>
+    <p>Sorry we did not find this page. Go back to <a href="/">home</a></p>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.container {
+  margin: 0 auto;
+  max-width: 60rem;
+  padding: 0 30px;
+  height: calc(100vh - 300px);
+  text-align: center;
+}
+
+a {
+  color: #373737;
+}
+</style>
