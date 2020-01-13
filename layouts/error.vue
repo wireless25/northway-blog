a01bd8602b7d92044ca7ff6c8cc6836aa1788a62
Stephan <stephan.simonett@gmail.com>
2020-01-13 21:36:40 +0100

    Add a custom error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,14 @@
 <template lang="html">
   <section class="container">
-    <h1>404</h1>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
     <p>Sorry we did not find this page. Go back to <a href="/">home</a></p>
   </section>
 </template>
 
 <script>
 export default {
+  props: ['error']
 }
 </script>
 
