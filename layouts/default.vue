cdb7859723c28db4008b69d02aad4fe5bda28c0c
Stephan <stephan.simonett@gmail.com>
2020-01-27 20:24:42 +0100

    Add page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,11 +24,38 @@ export default {
 <style>
 #layout {
   position: relative;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
 .main-content {
   margin: 4.5rem auto 0;
   padding-top: .1rem;
   overflow: auto;
+  flex: 1;
+}
+
+.page-enter, .page-leave-active {
+  opacity: 0;
+  transition: opacity .3s ease-in-out;
+  transform-origin: 50% 50%;
+}
+
+.page-enter-active {
+  animation: acrossIn .5s ease-in-out;
+  transition: opacity .8s ease-in-out;
+} 
+
+@keyframes acrossIn {
+  0% {
+    transform: translate3d(0, 3%, 0);
+  }
+  50% {
+    
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+  }
 }
 </style>
